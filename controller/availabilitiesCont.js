f9ebd45fe9a783f410a849951c245c3e9b6a9e4f
WanderingWanderer <proffesionalworkingstuff@gmail.com>
2024-09-09 20:07:25 +0200

    created a path for user roles to be filtered

--- controller/availabilitiesCont.js
+++ controller/availabilitiesCont.js
@@ -1,4 +1,4 @@
-import {getOpeningsDb, makeBookingDb} from '../model/availabilitiesDb.js'
+import {getOpeningsDb, makeBookingDb, showBookingsDb} from '../model/availabilitiesDb.js'
 const getOpenings = async(req,res)=>{
     
     res.json(await getOpeningsDb(req.params.id, req.params.month))
@@ -8,5 +8,9 @@ const makeBooking = async (req,res)=>{
     await makeBookingDb(recipient, requesting, app_date, slot_id, min_allocated, topic )
     res.send('alls good')
 }
+const showBookings = async (req,res)=>{
+// res.send('this works')
+    res.json(await showBookingsDb(req.params.id))
+}
 
-export {getOpenings, makeBooking}
+export {getOpenings, makeBooking, showBookings}
--- model/userDb.js
+++ model/userDb.js
@@ -4,6 +4,10 @@ const getUsersDb= async ()=>{
     let [data] = await pool.query('SELECT * FROM users')
     return data
 }
+const filteredRolesDb= async (role)=>{
+    let [data] = await pool.query('SELECT * FROM users where user_role =?',[role] )
+    return data
+}
 
 const getUserEmailDb = async (email)=>{
     let [[data]] = await pool.query('SELECT * FROM users WHERE email_add = ?',[email])
@@ -16,4 +20,4 @@ const getUserDb = async (id)=>{
 const registerUserDb = async(first_name, middle_name, last_name, user_role, email_add, user_pass)=>{
     await pool.query('INSERT INTO users (first_name, middle_name, last_name, user_role, email_add, user_pass) VALUES (?,?,?,?,?,?);',[first_name, middle_name, last_name, user_role, email_add, user_pass])
 }
-export {getUsersDb, getUserDb, getUserEmailDb, registerUserDb}
+export {getUsersDb, getUserDb, getUserEmailDb, registerUserDb, filteredRolesDb}
